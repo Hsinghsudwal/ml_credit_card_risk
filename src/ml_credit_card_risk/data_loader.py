14821ea3979aa08830302852bb514309ea54a434
Harinder Singh Sudwal <sudwalh@gmail.com>
2025-02-13 17:45:21 -0800

    loader and validate

---
+++ src/ml_credit_card_risk/data_loader.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import os
+from src.constants import ARTIFACT, RAW_PATH, TEST_SIZE, TRAIN, TEST
+from sklearn.model_selection import train_test_split
+
+
+class DataLoader:
+
+    def __init__(self) -> None:
+        pass
+
+    def data_load(self, path):
+
+        try:
+            # data=NOTEBOOK_DATA
+
+            df=pd.read_csv(path)
+            df.columns=df.columns.str.strip()
+
+            # split into train test data
+            train_data, test_data = train_test_split(df, test_size= TEST_SIZE, random_state=42)
+
+            raw_data_path = os.path.join(ARTIFACT, RAW_PATH)
+            os.makedirs(raw_data_path, exist_ok=True)
+            # save train test to csv
+            train_data.to_csv(os.path.join(raw_data_path, str(TRAIN)), index=False)
+            test_data.to_csv(os.path.join(raw_data_path, str(TEST)), index=False)
+
+            return train_data, test_data
+        
+        except Exception as e:
+              raise e
+
+
+    
---
+++ src/ml_credit_card_risk/data_validate.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from scipy.stats import ks_2samp
+from src.constants import ARTIFACT,  VALIDATE_PATH, TRAIN_VAL, TEST_VAL
+import os
+import json
+
+
+class DataValidation:
+
+    def __init__(self) -> None:
+        pass
+
+    def data_validation(self, train_data, test_data):
+        try:
+
+            for feature in train_data.columns:
+                ks_stat, p_value = ks_2samp(train_data[feature], test_data[feature])
+
+                if p_value < 0.05:
+                    return "Error drift data"
+
+                else:
+                    validate_data_path = os.path.join(ARTIFACT, VALIDATE_PATH)
+                    os.makedirs(validate_data_path, exist_ok=True)
+                    # save train test to csv
+                    train_data.to_csv(
+                        os.path.join(validate_data_path, str(TRAIN_VAL)),
+                        index=False,
+                    )
+                    test_data.to_csv(
+                        os.path.join(validate_data_path, str(TEST_VAL)),
+                        index=False,
+                    )
+                    
+                    return train_data, test_data
+
+        except Exception as e:
+            raise e
