f220a7fe2e5fb3c602571cc28fa376f4a031d927
Harinder Singh Sudwal <sudwalh@gmail.com>
2025-02-18 14:25:32 -0800

    outputs

---
+++ src/constants.py
@@ -0,0 +1,39 @@
+# BASE:
+PROJECT_NAME = "Credit Card Risk"
+AUTHOR = "Harinder Singh Sudwal"
+
+# DATA_LOCATION_PATH
+ARTIFACT = "artifact"
+RAW_PATH = "raw"
+TRAIN = "train.csv"
+TEST = "test.csv"
+
+# Data Validate
+VALIDATE_PATH = "validate"
+TRAIN_VAL = "vali_train.csv"
+TEST_VAL = "vali_test.csv"
+
+# Data Transformation
+TRANSFORM = "transform"
+XTRAIN_TRANS = "x_train.csv"
+XTEST_TRANS = "x_test.csv"
+YTRAIN_TRANS = "y_train.csv"
+YTEST_TRANS = "y_test.csv"
+
+# Model Trainer
+MODEL = "model"
+CLF = "model.joblib"
+
+# Evaluate
+EVALUATE = "evaluate"
+METRIC = "metric.json"
+CLASS_REPORT = "classification_report.txt"
+MATRIX = "matric.png"
+
+# Experiment
+EXPERIMENT = "experiment"
+EXPERIMENT_NAME = "credit_risk"
+MODEL_NAME = "xgboost"
+ARCHIVED = "Archived"
+STAGE = "Staging"
+PROD = "Production"
