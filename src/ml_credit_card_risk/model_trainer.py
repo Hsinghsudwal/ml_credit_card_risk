511156a44ff2e91dfab45f11c7ad7983f4d4f5a9
Harinder Singh Sudwal <sudwalh@gmail.com>
2025-02-18 14:27:00 -0800

    model trainer edit

---
+++ src/ml_credit_card_risk/model_trainer.py
@@ -0,0 +1,52 @@
+import os
+import joblib
+import pandas as pd
+from xgboost import XGBClassifier
+from src.constants import ARTIFACT, MODEL, CLF
+from sklearn.model_selection import GridSearchCV
+
+
+class ModelTrainer:
+    def __init__(self) -> None:
+        pass
+
+    def model_trainer(self, X_train, y_train):
+        try:
+            model = XGBClassifier()
+
+            # Define the parameter grid
+            param_grid = {
+                "learning_rate": [0.01, 0.05, 0.1],
+                "max_depth": [3, 6, 10],
+                "n_estimators": [50, 100, 150],
+                "subsample": [0.7, 0.8, 1.0],
+                "colsample_bytree": [0.7, 0.8, 1.0],
+            }
+
+            grid_search = GridSearchCV(
+                estimator=model,
+                param_grid=param_grid,
+                cv=3,
+                n_jobs=-1,
+                verbose=1,
+                scoring="accuracy",
+            )
+
+            # Perform Grid Search
+            grid_search.fit(X_train, y_train)
+
+            best_params = grid_search.best_params_
+
+            best_model = grid_search.best_estimator_
+
+            model_path = os.path.join(ARTIFACT, MODEL)
+            os.makedirs(model_path, exist_ok=True)
+
+            model_filename = os.path.join(model_path, CLF)
+            # joblib.dump((best_model, best_params), model_filename)
+            joblib.dump({"model": best_model, "params": best_params}, model_filename)
+
+            return best_model, best_params
+
+        except Exception as e:
+            raise e
